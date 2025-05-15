dc723dfce317a3dcfff3f2bec5b82b8bdb2b7dcf
Felipe Franco <felipejf739@hotmail.com>
2025-05-15 10:06:41 -0300

    funcionarios / test

--- PadroesProjeto/src/main/java/padroescomportamentais/chainofresponsability/Funcionario.java
+++ PadroesProjeto/src/main/java/padroescomportamentais/chainofresponsability/Funcionario.java
@@ -3,7 +3,18 @@ package padroescomportamentais.chainofresponsability;
 import java.util.ArrayList;
 
 public abstract class Funcionario {
-    private ArrayList documentos = new ArrayList();
+    protected ArrayList listaDocumentos = new ArrayList();
+    private Funcionario funcionarioSuperior;
+
+    public Funcionario getFuncionarioSuperior() {
+        return funcionarioSuperior;
+    }
+
+    public void setFuncionarioSuperior(Funcionario funcionarioSuperior) {
+        this.funcionarioSuperior = funcionarioSuperior;
+    }
+
+    public abstract String getDescricaoCargo();
 
 
 
--- PadroesProjeto/src/main/java/padroescomportamentais/chainofresponsability/FuncionarioSecretaria.java
+++ PadroesProjeto/src/main/java/padroescomportamentais/chainofresponsability/FuncionarioSecretaria.java
@@ -2,6 +2,13 @@ package padroescomportamentais.chainofresponsability;
 
 public class FuncionarioSecretaria extends Funcionario{
 
+    public FuncionarioSecretaria(Funcionario superior) {
+        listaDocumentos.add(TipoDocumentoHistorico.getTipoDocumentoHistorico());
+        setFuncionarioSuperior(superior);
+    }
 
+    public String getDescricaoCargo() {
+        return "Secretaria";
+    }
 
 }
